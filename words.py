c3ce7b13c6902c95977920229c976ac1dceca9a5
amisha229 <singhamisha4002@gmail.com>
2024-12-23 02:51:14 +0530

    Initial commit of Simple Hangman Game

---
+++ words.py
@@ -0,0 +1,7 @@
+word_list = [
+    "algorithm", "binary", "cache", "cloud", "compile", "cookie", 
+    "data", "debug", "disk", "encryption", "firewall", "hardware",
+    "internet", "keyboard", "laptop", "logic", "memory", "network",
+    "password", "processor", "python", "router", "server", "software",
+    "storage", "syntax", "variable", "virtual", "wireless"
+]
